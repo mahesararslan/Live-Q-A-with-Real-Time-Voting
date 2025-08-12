eba43bc1afb77dcdc964174672432fc880478e1b
Arslan Mahesar <mahesararslan1998@gmail.com>
2025-08-13 00:23:39 +0500

    Adds Websocket auth

--- backend/src/events/events.gateway.ts
+++ backend/src/events/events.gateway.ts
@@ -1,13 +1,21 @@
 import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { ServerToClientEvents } from './types/events';
 import { Question } from 'src/entities/question.entity';
+import { UseGuards } from '@nestjs/common';
+import { WsJwtGuard } from 'src/auth/guards/ws-jwt/ws-jwt.guard';
+import { SocketAuthMiddleware } from 'src/auth/guards/ws-jwt/ws.mw';
 
 @WebSocketGateway({ namespace: '/events' })
+@UseGuards(WsJwtGuard)
 export class EventsGateway {
 
   @WebSocketServer()
   server: Server<any, ServerToClientEvents>;
+
+  afterInit(client: Socket) {
+    client.use(SocketAuthMiddleware() as any);
+  }
 
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
